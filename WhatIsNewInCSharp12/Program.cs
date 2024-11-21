84c3eeb59ea17b195ac872a772dea97b248d2e2f
darvishiparham14@gmail.com <darvishiparham14@gmail.com>
2024-11-21 20:54:46 +0330

    Add project files.

---
+++ WhatIsNewInCSharp12/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace WhatIsNewInCSharp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Collection Expressions
+            //Old Version
+            char[] vowelsOld = { 'a', 'b', 'c' };
+            //New Version
+            char[] vowelsNew = ['a', 'b', 'c'];
+            #endregion
+        }
+    }
+}
